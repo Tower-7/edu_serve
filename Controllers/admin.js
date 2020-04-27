c9b4fefee0604c34cc2052d138f4a9497970de67
tower-7 <982155153@qq.com>
2020-04-27 09:54:53 +0800

    pull

--- Controllers/admin.js
+++ Controllers/admin.js
@@ -3,13 +3,11 @@ const Admin = require('../Models/admin')
 module.exports = {
     //主页
     index: async ctx => {
-        await ctx.render('admin/index',{
-            
-        })
+        await ctx.render('view/index.html')
     },
     // 注册
     sign_up: async ctx => {
         await Common.update(ctx,Admin)
     }
-    
+
 }
